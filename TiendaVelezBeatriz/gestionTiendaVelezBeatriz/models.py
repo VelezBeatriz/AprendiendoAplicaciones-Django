ae29213fa745c463d209a117f2aacf4c09a3e229
Beatriz Vélez <velezsanchezbeatriz@fpllefia.com>
2023-11-09 17:33:18 +0100

    Añadir __str__ en las clases

--- TiendaVelezBeatriz/gestionTiendaVelezBeatriz/models.py
+++ TiendaVelezBeatriz/gestionTiendaVelezBeatriz/models.py
@@ -7,6 +7,9 @@ class Fabricante(models.Model):
     nombre=models.CharField(max_length=30)
     apellidos=models.CharField(max_length=30)
     email=models.EmailField()
+    
+    def __str__(self):
+        return 'El fabricante se llama %s %s y su correo electrónico es %s' % (self.nombre, self.apellidos, self.email)
 
 # Cada clase posteriormente será una tabla
 class Clientes(models.Model):
@@ -15,15 +18,30 @@ class Clientes(models.Model):
     email=models.EmailField()
     telefono=models.CharField(max_length=7)
 
+    def __str__(self):
+        return 'El cliente se llama %s, vive en %s. Puedes contactarlo desde %s o %s' % (self.nombre, self.direccion, self.email, self.telefono)
+
+    
 class Articulos(models.Model):
     nombre=models.CharField(max_length=30)
     seccion=models.CharField(max_length=20)
     precio=models.IntegerField()
     fabricante=models.ForeignKey(Fabricante, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return 'Fabricante del pedido %s %s.El nombre es %s, la sección es %s y el precio es %s' % (self.fabricante.nombre, self.fabricante.apellidos, self.nombre, self.seccion, self.precio)
+
 class Pedidos(models.Model):
     numero=models.IntegerField()
     fecha=models.DateField()
     entregador=models.BooleanField()
+
+    def __str__(self):
+        if self.entregador:
+            return 'El pedido nº%s ha sido entregado el día %s' % (self.numero, self.fecha)
+        else:
+            return 'El pedido no ha sido entregado'
+
+   
 
 
