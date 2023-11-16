f368ba488180bc4bf5580c4d53902504e5e86e90
Beatriz Vélez <145451383+VelezBeatriz@users.noreply.github.com>
2023-11-16 16:56:21 +0100

    Update administration panel

--- TiendaVelezBeatriz/gestionTiendaVelezBeatriz/admin.py
+++ TiendaVelezBeatriz/gestionTiendaVelezBeatriz/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from gestionTiendaVelezBeatriz.models import Clientes, Fabricante ,Articulos, Pedidos
+
+#Create list display administration panel
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=("nombre", "email", "telefono")
+    search_fields=("email", "telefono")
+
+class FabricanteAdmin(admin.ModelAdmin):
+    list_display=("nombre", "apellidos", "email")
+    search_fields=("nombre", "apellidos")
+
+class ArticulosAdmin(admin.ModelAdmin):
+    list_display=("nombre", "seccion", "precio")
+    search_fields=("nombre", "seccion", "precio")
+
+class PedidoseAdmin(admin.ModelAdmin):
+    list_display=("numero", "fecha", "entregador")
+    search_fields=("numero", "fecha", "entregador")
+
+
+# Register models on database
+admin.site.register(Clientes, ClientesAdmin)
+admin.site.register(Fabricante, FabricanteAdmin)
+admin.site.register(Articulos, ArticulosAdmin)
+admin.site.register(Pedidos, PedidoseAdmin)
+
+
+
+
--- TiendaVelezBeatriz/gestionTiendaVelezBeatriz/models.py
+++ TiendaVelezBeatriz/gestionTiendaVelezBeatriz/models.py
@@ -4,43 +4,43 @@ from django.db import models
 
 # Creacion de la classe fabricante
 class Fabricante(models.Model):
-    nombre=models.CharField(max_length=30)
-    apellidos=models.CharField(max_length=30)
-    email=models.EmailField()
+    nombre=models.CharField(max_length=30, verbose_name="Nombre del fabricante")
+    apellidos=models.CharField(max_length=30, verbose_name="Primer y segundo apellido")
+    email=models.EmailField(verbose_name="Correo electrónico")
     
     def __str__(self):
-        return 'El fabricante se llama %s %s y su correo electrónico es %s' % (self.nombre, self.apellidos, self.email)
+        return 'El fabricante  %s %s tiene este correo electrónico %s' % (self.nombre, self.apellidos, self.email)
 
 # Cada clase posteriormente será una tabla
 class Clientes(models.Model):
-    nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
-    email=models.EmailField()
-    telefono=models.CharField(max_length=7)
+    nombre=models.CharField(max_length=30, verbose_name="Nombre del cliente")
+    direccion=models.CharField(max_length=50, verbose_name="Dirección")
+    email=models.EmailField(blank=True, null=True, verbose_name="Correo electrónico" )
+    telefono=models.CharField(max_length=7, verbose_name="Teléfono")
 
     def __str__(self):
-        return 'El cliente se llama %s, vive en %s. Puedes contactarlo desde %s o %s' % (self.nombre, self.direccion, self.email, self.telefono)
+        return 'El cliente %s, vive en %s. Sus contactos son %s o %s' % (self.nombre, self.direccion, self.email, self.telefono)
 
     
 class Articulos(models.Model):
-    nombre=models.CharField(max_length=30)
-    seccion=models.CharField(max_length=20)
-    precio=models.IntegerField()
-    fabricante=models.ForeignKey(Fabricante, on_delete=models.CASCADE)
+    nombre=models.CharField(max_length=30,verbose_name="Nombre del artículo")
+    seccion=models.CharField(max_length=20,verbose_name="Sección")
+    precio=models.IntegerField(verbose_name="Precio en €")
+    fabricante=models.ForeignKey(Fabricante, on_delete=models.CASCADE, verbose_name="Identificador del fabricante")
 
     def __str__(self):
-        return 'Fabricante del pedido %s %s.El nombre es %s, la sección es %s y el precio es %s' % (self.fabricante.nombre, self.fabricante.apellidos, self.nombre, self.seccion, self.precio)
+        return 'El fabricante del pedido es %s %s.El articulo es %s, su sección es %s y su precio es %s' % (self.fabricante.nombre, self.fabricante.apellidos, self.nombre, self.seccion, self.precio)
 
 class Pedidos(models.Model):
-    numero=models.IntegerField()
-    fecha=models.DateField()
-    entregador=models.BooleanField()
+    numero=models.IntegerField(verbose_name="Número del pedido")
+    fecha=models.DateField(verbose_name="Fecha de entrega")
+    entregador=models.BooleanField(verbose_name="Entregado o no")
 
     def __str__(self):
         if self.entregador:
             return 'El pedido nº%s ha sido entregado el día %s' % (self.numero, self.fecha)
         else:
-            return 'El pedido no ha sido entregado'
+            return 'El pedido nº%s no ha sido entregado' % (self.numero)
 
    
 
